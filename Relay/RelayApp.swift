bea3e8e702b166e7aca75a812df02724bc2b13b2
Tyler Dailey <tkdailey11@users.noreply.github.com>
2026-09-15 20:35:23 -0600

    Initial Commit

---
+++ Relay/RelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelayApp.swift
+//  Relay
+//
+//  Created by Tyler Dailey on 9/15/26.
+//
+
+import SwiftUI
+
+@main
+struct RelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RelayTests/RelayTests.swift
@@ -0,0 +1,17 @@
+//
+//  RelayTests.swift
+//  RelayTests
+//
+//  Created by Tyler Dailey on 9/15/26.
+//
+
+import Testing
+@testable import Relay
+
+struct RelayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
